55f729d4586db4e245cd053155e71f3bf9a0f9dc
yevgeniy.shaidur <insight2pizzza@gmail.com>
2022-01-27 20:50:21 +0200

    update userDto

--- src/main/java/com/app/crudapp/dto/UserDto.java
+++ src/main/java/com/app/crudapp/dto/UserDto.java
@@ -1,9 +1,12 @@
 package com.app.crudapp.dto;
 
+import com.app.crudapp.model.Role;
 import com.app.crudapp.model.Status;
 import com.app.crudapp.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -11,12 +14,14 @@ public class UserDto {
     private Integer id;
     private String name;
     private Status status;
+    private List<Role> roles;
 
     public static UserDto fromUser(User user) {
         UserDto userDto = new UserDto();
         userDto.setId(user.getId());
         userDto.setName(user.getName());
         userDto.setStatus(user.getStatus());
+        userDto.setRoles(user.getRoles());
 
         return userDto;
     }
