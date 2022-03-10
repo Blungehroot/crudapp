c2d51f66090ea88a4f187cb814517caa4617e71c
yevgeniy.shaidur <insight2pizzza@gmail.com>
2022-03-10 20:22:16 +0200

    impl base aws classes and add env variable for aws

--- src/main/java/com/app/crudapp/aws/AwsClient.java
+++ src/main/java/com/app/crudapp/aws/AwsClient.java
@@ -3,21 +3,24 @@ package com.app.crudapp.aws;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 
-public class AwsClient {
+public final class AwsClient {
     @Value("${aws.access.key}")
-    private String awsAccessKey;
+    private static String awsAccessKey;
     @Value("${aws.secret.key}")
-    private String awsSecretKey;
+    private static String awsSecretKey;
 
-    AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+    public static AmazonS3 getS3Connection() {
+        AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
 
-    AmazonS3 amazonS3 = AmazonS3ClientBuilder
-            .standard()
-            .withCredentials(new AWSStaticCredentialsProvider(credentials))
-            .withRegion(Regions.)
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+    }
 }
